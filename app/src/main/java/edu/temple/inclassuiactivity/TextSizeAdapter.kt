bc4062104d0c5a6083ff54a413f1c29766531f2d
Mr-Sonntag <personalzbiz@gmail.com>
2026-09-15 15:13:40 -0400

    Initial commit

--- app/src/main/java/edu/temple/inclassuiactivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiactivity/TextSizeAdapter.kt
@@ -1,25 +1,57 @@
 package edu.temple.inclassuiactivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _fonts: Array<Int>) : BaseAdapter() {
+//    Assigning _context to a value and fonts
+    private val context = _context
+    private val fonts = _fonts
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return fonts.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return fonts[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?) = (if(convertView==null)
+                TextView(context)
+            else {
+        convertView as TextView
+    }).apply{
+                    text = fonts[position].toString()
+                    textSize = 22f
+    }
+
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+////        val textView = TextView(context)
+////        textView.text = fonts[position].toString()
+////        textView.textSize = fonts[position].toFloat()
+//
+//        val textView = if(convertView==null)
+//            TextView(context)
+//        else{
+//            (convertView as TextView)}
+//        textView.text = fonts[position].toString()
+//        textView.textSize = 22f
+//
+//        return textView
+//    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val textView = (getView(position,convertView,parent) as TextView)
+        textView.textSize = fonts[position].toFloat()
+
+        return textView
     }
 
 }
